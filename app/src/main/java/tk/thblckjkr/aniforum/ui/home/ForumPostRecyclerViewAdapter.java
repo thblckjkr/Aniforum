95aba60b4e726bc39cab8a55a60767db4973b877
thblckjkr <teo.gonzalez.c@hotmail.com>
2020-11-08 22:13:12 -0700

    Updated icon and added user avatar

--- app/src/main/java/tk/thblckjkr/aniforum/ui/home/ForumPostRecyclerViewAdapter.java
+++ app/src/main/java/tk/thblckjkr/aniforum/ui/home/ForumPostRecyclerViewAdapter.java
@@ -2,10 +2,15 @@ package tk.thblckjkr.aniforum.ui.home;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.koushikdutta.ion.Ion;
 
 import tk.thblckjkr.aniforum.R;
 import tk.thblckjkr.aniforum.models.ForumPosts;
@@ -28,9 +33,25 @@ public class ForumPostRecyclerViewAdapter extends RecyclerView.Adapter<ForumPost
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
 //        holder.mItem = mPosts.get(0).;
+        String body = mPosts.posts().get(position).body;
+        body = body.length() > 256 ? body.substring(0, 256) + "..." : body;
+
         holder.mPostTitleView.setText(mPosts.posts().get(position).title);
-        holder.mPostBodyView.setText(mPosts.posts().get(position).body);
-        holder.mPostUserView.setText(mPosts.posts().get(position).user.name);
+        holder.mPostBodyView.setText(body);
+        holder.mPostUserView.setText("u/" + mPosts.posts().get(position).user.name);
+
+        holder.mPostImage.setVisibility(View.GONE);
+
+        try {
+            Ion.with(holder.mPostUserAvatar)
+                    .placeholder(R.drawable.hourglass_empty)
+                    .error(R.drawable.broken_image)
+//                .animateLoad(  )
+//                .animateIn(fadeInAnimation)
+                    .load(mPosts.posts().get(position).user.avatar.medium());
+        }catch (Exception e) {
+
+        }
     }
 
     @Override
@@ -43,6 +64,8 @@ public class ForumPostRecyclerViewAdapter extends RecyclerView.Adapter<ForumPost
         public final TextView mPostTitleView;
         public final TextView mPostBodyView;
         public final TextView mPostUserView;
+        public final ImageView mPostUserAvatar;
+        public final ImageView mPostImage;
 
         public ViewHolder(View view) {
             super(view);
@@ -50,6 +73,8 @@ public class ForumPostRecyclerViewAdapter extends RecyclerView.Adapter<ForumPost
             mPostTitleView = (TextView) view.findViewById(R.id.post_title);
             mPostBodyView = (TextView) view.findViewById(R.id.post_body);
             mPostUserView = (TextView) view.findViewById(R.id.post_author);
+            mPostImage = (ImageView) view.findViewById(R.id.post_image);
+            mPostUserAvatar = (ImageView) view.findViewById(R.id.user_avatar);
         }
 
         @Override
